f2eb169a6f8f155a63beaa85a7acccfec6eaeb95
semraincedayi <96700930+semraincedayi@users.noreply.github.com>
2022-05-17 01:00:15 +0300

    commit semra

--- Yazilim Yapimi Proje/Yazilim Yapimi Proje/kayit.Designer.cs
+++ Yazilim Yapimi Proje/Yazilim Yapimi Proje/kayit.Designer.cs
@@ -42,20 +42,25 @@ namespace Yazilim_Yapimi_Proje
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(11, 76);
+            this.label1.Location = new System.Drawing.Point(8, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 22);
+            this.label1.Size = new System.Drawing.Size(32, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "Adı";
             // 
@@ -63,9 +68,10 @@ namespace Yazilim_Yapimi_Proje
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(11, 108);
+            this.label2.Location = new System.Drawing.Point(8, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 22);
+            this.label2.Size = new System.Drawing.Size(54, 19);
             this.label2.TabIndex = 0;
             this.label2.Text = "Soyadı";
             // 
@@ -73,9 +79,10 @@ namespace Yazilim_Yapimi_Proje
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(11, 145);
+            this.label3.Location = new System.Drawing.Point(8, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 22);
+            this.label3.Size = new System.Drawing.Size(94, 19);
             this.label3.TabIndex = 0;
             this.label3.Text = "Kullanıcı Adı";
             // 
@@ -83,9 +90,10 @@ namespace Yazilim_Yapimi_Proje
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(11, 178);
+            this.label4.Location = new System.Drawing.Point(8, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 22);
+            this.label4.Size = new System.Drawing.Size(41, 19);
             this.label4.TabIndex = 1;
             this.label4.Text = "Şifre";
             // 
@@ -93,9 +101,10 @@ namespace Yazilim_Yapimi_Proje
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(11, 215);
+            this.label6.Location = new System.Drawing.Point(11, 238);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 22);
+            this.label6.Size = new System.Drawing.Size(40, 19);
             this.label6.TabIndex = 3;
             this.label6.Text = "Soru";
             // 
@@ -103,66 +112,62 @@ namespace Yazilim_Yapimi_Proje
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(11, 250);
+            this.label5.Location = new System.Drawing.Point(7, 263);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 22);
+            this.label5.Size = new System.Drawing.Size(52, 19);
             this.label5.TabIndex = 3;
             this.label5.Text = "Cevap";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 75);
+            this.textBox1.Location = new System.Drawing.Point(135, 61);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(180, 103);
+            this.textBox2.Location = new System.Drawing.Point(135, 84);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.Size = new System.Drawing.Size(92, 20);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(180, 145);
+            this.textBox3.Location = new System.Drawing.Point(135, 118);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 22);
+            this.textBox3.Size = new System.Drawing.Size(92, 20);
             this.textBox3.TabIndex = 4;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(180, 178);
+            this.textBox4.Location = new System.Drawing.Point(135, 145);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 22);
+            this.textBox4.Size = new System.Drawing.Size(92, 20);
             this.textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(180, 250);
+            this.textBox5.Location = new System.Drawing.Point(135, 177);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(121, 22);
+            this.textBox5.Size = new System.Drawing.Size(92, 20);
             this.textBox5.TabIndex = 4;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "İlk araban",
-            "İlk öğretmenin "});
-            this.comboBox1.Location = new System.Drawing.Point(180, 213);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 5;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label7.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.label7.Location = new System.Drawing.Point(73, 14);
+            this.label7.Location = new System.Drawing.Point(55, 11);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(285, 22);
+            this.label7.Size = new System.Drawing.Size(258, 19);
             this.label7.TabIndex = 3;
             this.label7.Text = "Kayıt Olma Ekranına Hoşgeldiniz";
             // 
@@ -173,8 +178,9 @@ namespace Yazilim_Yapimi_Proje
             this.button3.ImageIndex = 0;
             this.button3.ImageList = this.ımageList1;
             this.button3.Location = new System.Drawing.Point(2, -1);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(49, 48);
+            this.button3.Size = new System.Drawing.Size(37, 39);
             this.button3.TabIndex = 20;
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button3.UseVisualStyleBackColor = true;
@@ -191,19 +197,72 @@ namespace Yazilim_Yapimi_Proje
             this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.ImageIndex = 0;
-            this.button1.Location = new System.Drawing.Point(258, 287);
+            this.button1.Location = new System.Drawing.Point(265, 332);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.Size = new System.Drawing.Size(92, 28);
             this.button1.TabIndex = 20;
             this.button1.Text = "Kayıt Ol";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "İlk araban",
+            "İlk öğretmenin "});
+            this.comboBox1.Location = new System.Drawing.Point(135, 238);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(135, 264);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(92, 20);
+            this.textBox6.TabIndex = 23;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(135, 204);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(92, 20);
+            this.textBox7.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(12, 178);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 19);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Mail";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(12, 203);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 19);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "ID";
+            // 
             // kayit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-            this.ClientSize = new System.Drawing.Size(392, 334);
+            this.ClientSize = new System.Drawing.Size(368, 382);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.comboBox1);
@@ -219,8 +278,10 @@ namespace Yazilim_Yapimi_Proje
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "kayit";
             this.Text = "Kayıt Olmaz Ekranı";
+            this.Load += new System.EventHandler(this.kayit_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,10 +300,14 @@ namespace Yazilim_Yapimi_Proje
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ImageList ımageList1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
     }
 }
--- Yazilim Yapimi Proje/Yazilim Yapimi Proje/kayit.cs
+++ Yazilim Yapimi Proje/Yazilim Yapimi Proje/kayit.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Yazilim_Yapimi_Proje
 {
@@ -25,6 +26,39 @@ namespace Yazilim_Yapimi_Proje
             this.Dispose();
             
             
+        }
+        public static SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-G8KPTIR;Initial Catalog=Kullanicilar;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection();
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();
+                string kayit = "INSERT INTO Kullanici(KullaniciID,KullaniciAdi,Ad,Soyad,Mail,Sifre)values(@id,@kullaniciad,@ad,@soyad,@mail,@sifre)";
+                SqlCommand komut = new SqlCommand(kayit, baglanti);
+                komut.Parameters.AddWithValue("@ad", textBox1.Text);
+                komut.Parameters.AddWithValue("@soyad", textBox2.Text);
+                komut.Parameters.AddWithValue("@kullaniciad", textBox3.Text);
+                komut.Parameters.AddWithValue("@sifre", textBox4.Text);
+                komut.Parameters.AddWithValue("@mail", textBox5.Text);
+                komut.Parameters.AddWithValue("@id", textBox7.Text);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt İşlemi Gerçekleştirilmiştir");
+
+
+
+            }
+            catch(Exception hata)
+            {
+                MessageBox.Show("Kayıt Gerçekleştirilememiştir");
+            }
+        }
+
+        private void kayit_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
